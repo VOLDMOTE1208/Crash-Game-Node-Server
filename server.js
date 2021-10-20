56cbbefda99fa6b3c983eddb4ea448a09d02b745
VOLDMOTE1213 <kkopbusiness1213@gmail.com>
2021-10-20 15:47:30 +0800

    add startGame

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ app.use('/api', routes);
 
 // Start server
 // server.listen(port);
+
+var isRunning = false;
+var endGame = false;
+var start_time = 6;
+var currentAmount = 1;
 server.listen(port, function(){
 	console.log('listening on *:' + port + '--- server is running ...');
 });
@@ -34,21 +39,52 @@ gameSocket = io.on('connection', function(socket){
       socket.emit('user list', req);
     });
 
-    console.log("user connected")
     console.log('socket connected: ' + socket.id);    
     socket.emit('connected', {});
+    
+    timeoutObj = setInterval(() => {
+      if(!isRunning)
+        socket.emit('startTime', {timeCount:start_time});
+    }, 1000);
 
-	/////////////////////////////////
-  });
+    timeoutObj = setInterval(() => {
+      if(isRunning)
+        socket.emit('currentAmount', {currentAmount:currentAmount});
+    }, 100);
+    	/////////////////////////////////
+  });  
+  
+  function startTime(){     
+    console.log(start_time)   
+    start_time--;
+    if(start_time==-1){
+      isRunning=true;
+      start_time=6;
 
-function guid() {
-	function s4() {
-		return Math.floor((1+Math.random()) * 0x10000).toString(16).substring(1);
-	}
-	return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+      if(isRunning){
+        var totalNum = (999999999/parseFloat(getRandomArbitrary(1,1000000000))).toFixed(2);
+        console.log(totalNum)
+        currentAmount=1;
+        timeoutObj = setInterval(() => {
+          // socket.emit('currentTime', {timeCount:i});
+          if(currentAmount.toFixed(2)!=totalNum){
+            console.log(currentAmount.toFixed(2))
+            currentAmount+=0.01;
+          }
+          else if(currentAmount.toFixed(2)==totalNum){
+            isRunning=false;
+          }
+        },100);     
+      } 
+
+    }          
 }
 
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
-}
+  function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
 
+  timeoutObj = setInterval(() => {
+    if(!isRunning)
+      startTime()
+  }, 1000); 
